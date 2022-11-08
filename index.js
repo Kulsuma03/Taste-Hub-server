f3f7e2921fa9c4225a0d007ba8c359f1c6acddc8
Kulsuma Akter <kulsumams72@gmail.com>
2022-11-08 21:54:38 +0600

    reviw api post

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('tasteHub').collection('services');
+        const reviewCollection = client.db('tasteHub').collection('review');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -33,11 +34,29 @@ async function run() {
             res.send(services);
         });
 
-        app.get('/allservice/:id', async (req, res) => {
+        app.get('/details/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            console.log(review);
+            res.send(result);
+        });
+
+          // review api
+          app.get('/review', async (req, res) => {
+            const service = req.query.service
+            console.log(service);
+            
+            let query = {};
+            const cursor = orderCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
         });
 
     }
